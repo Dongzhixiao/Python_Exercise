57cf7efa50639c0a652678deccaa187f5e0a040e
Dongzhixiao <dongzhixiaodong@gmail.com>
2018-02-03 16:56:21 +0800

    add associate program

---
+++ associate/自然论文关联分析.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 22:01:57 2018
+
+@author: Dongzhixiao
+"""
+
+import pandas as pd
+import datetime   #用来计算日期差的包
+import orangecontrib.associate.fpgrowth as oaf  #进行关联规则分析的包
+
+def dataInterval(data1,data2):
+    d1 = datetime.datetime.strptime(data1, '%Y-%m-%d')
+    d2 = datetime.datetime.strptime(data2, '%Y-%m-%d')
+    delta = d1 - d2
+    return delta.days
+
+def getInterval(arrLike):  #用来计算日期间隔天数的调用的函数
+    PublishedTime = arrLike['PublishedTime']
+    ReceivedTime = arrLike['ReceivedTime']
+#    print(PublishedTime.strip(),ReceivedTime.strip())
+    days = dataInterval(PublishedTime.strip(),ReceivedTime.strip())  #注意去掉两端空白
+    return days
+
+def dealRules(rules):
+    returnRules = []
+    for i in rules:
+        temStr = '';
+        for j in i[0]:   #处理第一个frozenset
+            temStr = temStr+j+'&'
+        temStr = temStr[:-1]
+        temStr = temStr + ' ==> '
+        for j in i[1]:
+            temStr = temStr+j+'&'
+        temStr = temStr[:-1]
+        temStr = temStr + ';' +'\t'+str(i[2])+ ';' +'\t'+str(i[3])
+#        print(temStr)
+        returnRules.append(temStr)
+    return returnRules
+
+def dealResult(rules):
+    returnRules = []
+    for i in rules:
+        temStr = '';
+        for j in i[0]:   #处理第一个frozenset
+            temStr = temStr+j+'&'
+        temStr = temStr[:-1]
+        temStr = temStr + ' ==> '
+        for j in i[1]:
+            temStr = temStr+j+'&'
+        temStr = temStr[:-1]
+        temStr = temStr + ';' +'\t'+str(i[2])+ ';' +'\t'+str(i[3])+ ';' +'\t'+str(i[4])+ ';' +'\t'+str(i[5])+ ';' +'\t'+str(i[6])+ ';' +'\t'+str(i[7])
+#        print(temStr)
+        returnRules.append(temStr)
+    return returnRules
+
+def ResultDFToSave(rules):   #根据Qrange3关联分析生成的规则得到并返回对于的DataFrame数据结构的函数
+    returnRules = []
+    for i in rules:
+        temList = []
+        temStr = '';
+        for j in i[0]:   #处理第一个frozenset
+            temStr = temStr + str(j) + '&'
+        temStr = temStr[:-1]
+        temStr = temStr + ' ==> '
+        for j in i[1]:
+            temStr = temStr + str(j) + '&'
+        temStr = temStr[:-1]
+        temList.append(temStr); temList.append(i[2]); temList.append(i[3]); temList.append(i[4])
+        temList.append(i[5]); temList.append(i[6]); temList.append(i[7])
+        returnRules.append(temList)
+    return pd.DataFrame(returnRules,columns=('规则','项集出现数目','置信度','覆盖度','力度','提升度','利用度'))
+    
+
+if __name__ == '__main__':    
+    fileName = "NS_new.xls";
+    df = pd.read_excel(fileName) 
+    df['TimeInterval'] = df.apply(getInterval , axis = 1)
+    listToAnalysis = []
+    listToStore = []
+    for i in range(df.iloc[:,0].size):             #df.iloc[:,0].size
+        #处理ArticleTag段位
+        s = df.iloc[i]['ArticleTag']
+        #s = re.sub('\s','_',s.split('/')[1].strip())
+        s = s.strip()
+        s = 'ArticleTag_'+s
+        listToStore.append(s)
+        #处理TimeInterval段位
+        s = df.iloc[i]['TimeInterval']
+        if s > 300:
+            s = 'TimeInterval_'+'300'
+        elif s > 200 and s <= 300:
+            s = 'TimeInterval_'+'200_300'
+        elif s > 100 and s <= 200:
+            s = 'TimeInterval_'+'100_200'
+        elif s <= 100:
+            s = 'TimeInterval_'+'0_100'
+        listToStore.append(s)
+        #处理ReferencesNumber段位
+        s = df.iloc[i]['ReferencesNumber']
+        if s > 80:
+            s = 'ReferencesNumber'+'80'
+        elif s > 60 and s <= 80:
+            s = 'ReferencesNumber'+'60_80'
+        elif s > 40 and s <= 60:
+            s = 'ReferencesNumber'+'40_60'
+        elif s > 20 and s <= 40:
+            s = 'ReferencesNumber'+'20_40'
+        elif s > 0 and s <= 20:
+            s = 'ReferencesNumber'+'0_20'
+        listToStore.append(s)
+        #处理Country段位
+        s = df.iloc[i]['Country']
+        s = 'Country_'+s.strip()
+        listToStore.append(s)
+        #print(listToStore)
+        listToAnalysis.append(listToStore.copy())
+        listToStore.clear()
+   #开始进行关联分析     
+    itemsets = dict(oaf.frequent_itemsets(listToAnalysis, .02)) #这里设置支持度
+    rules = oaf.association_rules(itemsets, .5)   #这里设置置信度
+    rules = list(rules)
+    printRules = dealRules(rules)
+    result = list(oaf.rules_stats(rules, itemsets, len(listToAnalysis)))   #下面这个函数改变了rules和itemsets，把rules和itemsets置空！
+    printResult = dealResult(result)
+
+#################################################下面将结果保存成excel格式的文件    
+    dfToSave = ResultDFToSave(result)
+    dfToSave.to_excel('regular.xlsx')
+
+#######################################################下面是根据不同置信度和关联度得到关联规则数目
+    listTable = []
+    supportRate = 0.1
+    confidenceRate = 0.1
+    for i in range(9):
+        support = supportRate*(i+1)
+        listS = []
+        for j in range(9):
+            confidence = confidenceRate*(j+1)
+            itemsets = dict(oaf.frequent_itemsets(listToAnalysis, support))
+            rules = list(oaf.association_rules(itemsets, confidence))
+            listS.append(len(rules))
+        listTable.append(listS)    
+    dfList = pd.DataFrame(listTable,index = [supportRate*(i+1) for i in range(9)],columns=[confidenceRate*(i+1) for i in range(9)])
+    dfList.to_excel('regularNum.xlsx')
+
+####################################################################下面是测试pymining工具包的代码
+#from pymining import itemmining, assocrules, perftesting
+#transactions = (('a', 'b', 'c'), ('b'), ('a'), ('a', 'c', 'd'), ('b', 'c'), ('b', 'c'))
+#relim_input = itemmining.get_relim_input(transactions)
+#
+#
+#item_sets = itemmining.relim(relim_input, min_support=2)
+#
+#rules = assocrules.mine_assoc_rules(item_sets, min_support=2, min_confidence=0.5)
+
---
+++ deviationDetection_PCA/pca_analysis_log.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt  #用于类似matlab绘图的包
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+from sklearn import decomposition as skldec #用于主成分分析降维的包
+from mpl_toolkits.mplot3d import Axes3D #用于3D绘图的包
+import seaborn as sns   #用于高阶画图的包
+import pandas as pd  
+import numpy as np
+from sklearn import preprocessing #用于预处理的包
+import math   #用于计算开方
+from scipy.stats import norm  #用于计算标准正态分布的分位数
+
+def testPCA():
+    a = np.array([1,2,3,4,5])
+    
+def drawTypeCorrlation(fileName):
+    df = pd.read_excel(fileName)
+    #sns.linearmodels.corrplot(df)  #画相关性图
+    
+def caculateLogVector(fileName):
+     # 读取数据
+    df = pd.read_excel(fileName)
+    pca = skldec.PCA(n_components = 0.95)
+    pca.fit(df)   #主城分析时每一行是一个输入数据
+    result = pca.transform(df)
+    df_origin = pca.inverse_transform(result)
+    
+    ndarr = np.array(df)  #输入数据，这里每一行是一个输入数据
+    ndarr = preprocessing.scale(ndarr,with_std=False) #中心化数据
+    Y = ndarr.T #使得每一个列是一个输入数据
+    #result2 = pca.components_.dot(Y).T   #该结果等于使用pca的transform的结果
+    P = pca.components_.T   #原来公式里的P是主成分列向量组成的
+    Y_a = Y - P.dot(P.T).dot(Y)
+    Y_a_2norm_sqr = np.zeros(Y_a.shape[1]).reshape(1,Y_a.shape[1])
+    for i in range(Y_a.shape[1]):
+        Y_a_2norm_sqr[0,i] = np.linalg.norm(Y_a[:,i])**2
+        
+    #sns.distplot(Y_a_2norm_sqr);
+    #sns.tsplot(data=Y_a_2norm_sqr[0,:10000],err_style = "ci_bars",interpolate = False)
+    
+    x = np.linspace(1, Y_a.shape[1] , Y_a.shape[1])
+    plt.scatter(x,Y_a_2norm_sqr, s=2)
+    plt.xlabel(u'时间片',{'color':'r'})
+    plt.ylabel(u'Q值',{'color':'r'})
+    #plt.hold
+    lineToDraw = caculateQ_Alpha(df,pca,0.95)
+    plt.plot(x,[lineToDraw for i in range(Y_a.shape[1])],'-r',label = 'normal',linewidth=1)
+    plt.plot(x,[2*lineToDraw for i in range(Y_a.shape[1])],'-g',label = 'slight',linewidth=1)
+    plt.plot(x,[4*lineToDraw for i in range(Y_a.shape[1])],'-b',label = 'warning',linewidth=1)
+    plt.plot(x,[8*lineToDraw for i in range(Y_a.shape[1])],'-k',label = 'critical',linewidth=1)
+    plt.legend()
+    # return 异常对应的序号
+    
+#==============================================================================
+#     numListToReturn = {'Critical':[],
+#                        'Error':[],
+#                        'Warning':[],
+#                        'Slight':[],
+#                        'Normal':[]};
+#     for i in range(Y_a_2norm_sqr.shape[1]):
+#         if Y_a_2norm_sqr[0,i] > 8*lineToDraw:
+#             numListToReturn['Critical'].append((i,Y_a_2norm_sqr[0,i]))
+#         elif Y_a_2norm_sqr[0,i] <= 8*lineToDraw and Y_a_2norm_sqr[0,i] > 4*lineToDraw:
+#             numListToReturn['Error'].append((i,Y_a_2norm_sqr[0,i]))
+#         elif Y_a_2norm_sqr[0,i] <= 4*lineToDraw and Y_a_2norm_sqr[0,i] > 2*lineToDraw:
+#             numListToReturn['Warning'].append((i,Y_a_2norm_sqr[0,i]))
+#         elif Y_a_2norm_sqr[0,i] <= 2*lineToDraw and Y_a_2norm_sqr[0,i] > 1*lineToDraw:
+#             numListToReturn['Slight'].append((i,Y_a_2norm_sqr[0,i]))
+#         else:
+#             numListToReturn['Normal'].append((i,Y_a_2norm_sqr[0,i]))
+#==============================================================================
+    
+    numListToReturn = {'Abnormal':[],
+                       'Normal':[]};
+    for i in range(Y_a_2norm_sqr.shape[1]):
+        if Y_a_2norm_sqr[0,i] > lineToDraw:
+            numListToReturn['Abnormal'].append((i,Y_a_2norm_sqr[0,i]))
+        else:
+            numListToReturn['Normal'].append((i,Y_a_2norm_sqr[0,i]))
+        
+    return numListToReturn,df
+    
+def caculateQ_Alpha(df,pca,alpha):
+    #pca.fit(df)
+    r = pca.explained_variance_.size
+    pca = skldec.PCA()
+    pca.fit(df)
+    Phi_1 = 0
+    Phi_2 = 0
+    Phi_3 = 0
+    for i in range(r,pca.explained_variance_.size):
+        Phi_1 = Phi_1 + pca.explained_variance_[i]**1
+        Phi_2 = Phi_2 + pca.explained_variance_[i]**2
+        Phi_3 = Phi_3 + pca.explained_variance_[i]**3
+    h_0 = 1 - 2*Phi_1*Phi_3/(3*Phi_2**2)
+    C_alpha = norm.cdf(alpha)   #得到正态分布alpha的百分位数
+    delta2_Alpha = Phi_1*(C_alpha*math.sqrt(2*Phi_2*h_0**2)/Phi_1 + 1 +
+                          Phi_2*h_0*(h_0-1)/Phi_1**2)**(1/h_0) 
+    return delta2_Alpha
+
+def draw_histogram(ls):
+    pass
+
+def generateDrawDF(strList,df,AnomalousLine):    #得到可以进行绘画的数据
+    data = [];
+    for i in range(len(strList)):
+        levelList = AnomalousLine[strList[i]]  #得到等级对应的列表
+        for j in range(len(levelList)):
+            lineNum = levelList[j][0]  #得到行号
+            LineSeries = df.iloc[lineNum]  #得到行的序列
+            for k in range(int(LineSeries.size/4.0)):    #int(LineSeries.size/4),int(LineSeries.size/2)
+                #print(LineSeries[k],LineSeries.index[k],strList[i])
+                data.append((LineSeries[k],LineSeries.index[k],strList[i]))
+                #下面一行绝对不要用，DataFrame绝对不要动态加行列！！！！
+#                Drawdf.loc[Drawdf.iloc[:,0].size] = [LineSeries[k],LineSeries.index[k],strList[i]]     
+#        print('计算了一次')
+#    print(data)
+    Drawdf = pd.DataFrame(columns=('times','type','level'),data = data)
+    return Drawdf
+
+def getSum(arrLike):
+    labelList = [i for i in range(46)]
+    sumToReturn = 0
+    for i in labelList:
+        sumToReturn = sumToReturn + arrLike[i]
+    return sumToReturn
+
+if __name__ == '__main__':
+    FileName = "test.xlsx"
+#    drawTypeCorrlation(FileName)
+    [AnomalousLine,df] = caculateLogVector(FileName)
+
+#####################################################################下面是找到Q值最大的几行和正常的Q值平均进行比较
+#    listLevel = ['null' for i in range(df.shape[0])] ;  listQ_value = [i for i in range(df.shape[0])];
+#    listSecNum = [i for i in range(df.shape[0])]
+#    for key,value in AnomalousLine.items():
+#        for tup in value:
+#            lineNum = tup[0]
+#            Q_value = tup[1]
+#            listLevel[lineNum] = key 
+#            listQ_value[lineNum] = Q_value            
+#    df['Level'] = listLevel; df['Q_value'] = listQ_value; #df['SecNum'] = listSecNum    #增加三列——等级/Q值/秒数    
+#    df['typeSum'] = df.apply(getSum , axis = 1)
+#    normalMean = df[df['Level'].isin(['Normal'])].mean()    #选取Normal的行求平均值组成Series    
+#    largeNQ = df.nlargest(50,'Q_value').drop('Level',axis = 1)  #选取Q值最大的10个行组成DataFrame    
+#    result = largeNQ.sub(normalMean)
+#    result = result.T
+#    result.plot(kind = 'bar')
+#    sns.pairplot(df, vars=["type9","type10"], hue ="Level", diag_kind="kde" #kind='reg'
+#                 )
+#    
+#    sns.pairplot(df, vars=["type1","type7","type11"], hue ="Level", diag_kind="kde" #kind='reg'
+#                 )
+
+#    sns.pairplot(df, vars=["type4","type5","type6","type8","type10","type11","type12","type15",
+#                           "type16","type19"], hue ="Level", diag_kind="kde" #kind='reg'
+#                 )
+    
+####################################################################下面是使用循环将多个类型每GroupNum为一组绘制出来
+#    GroupNum = 3   
+#    TypeList = ['type'+str(i) for i in range(46) ]
+#    ListToDraw = []
+#    for i in range(len(TypeList)):
+#        ListToDraw.append(TypeList[i])
+#        if (i+1)%GroupNum == 0:
+#            #plt.figure()
+#            
+#            sns.pairplot(df, vars=ListToDraw, diag_kind="kde", hue = "Level" #kind='reg'
+#                 )
+#            ListToDraw.clear()
+
+            
+    
+    
+#####################################################################下面是Seaborn画图代码    
+    strList = ['Normal','Abnormal']
+#    strList = ['Normal','Critical']
+#    strList = ['Normal']
+    Drawdf = generateDrawDF(strList,df,AnomalousLine)
+    
+#    plt.figure()
+#    sns.boxplot(x="type", y="times", hue="level",
+#                     data=Drawdf, palette="Set2")
+#    plt.figure()
+#    result = sns.barplot(x="type", y="times", hue="level", ci = None , capsize=.1, #errwidth = 0,
+#                     data=Drawdf, palette="Set2")
+
+#    plt.figure()
+#    sns.violinplot(x="type", y="times", hue="level",
+#                     data=Drawdf, palette="Set2")
+
+    
